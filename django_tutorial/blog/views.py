dde865be09d580a342eabf9205e087f55d8aa081
Jonas G F Pettersson <jonas.pettersson@siemens.com>
2020-05-29 22:37:10 +0200

    Initial blog posts

--- django_tutorial/blog/views.py
+++ django_tutorial/blog/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Jonas Pettersson',
+        'title': 'Blog post 1',
+        'content': 'First post contents',
+        'date_posted': 'May 5, 2020'
+    },
+    {
+        'author': 'Tanja Pettersson',
+        'title': 'Blog post 2',
+        'content': 'Second post contents',
+        'date_posted': 'May 6, 2020'
+    }
+]
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
