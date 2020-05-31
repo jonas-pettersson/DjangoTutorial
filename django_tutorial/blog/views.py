79806981a3ce24e03fb7f192644321ce11e28a01
Jonas G F Pettersson <jonas.pettersson@siemens.com>
2020-05-31 13:18:20 +0200

    End of Part 5: posts connected to database, post registered with admin

--- django_tutorial/blog/views.py
+++ django_tutorial/blog/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Jonas Pettersson',
-        'title': 'Blog post 1',
-        'content': 'First post contents',
-        'date_posted': 'May 5, 2020'
-    },
-    {
-        'author': 'Tanja Pettersson',
-        'title': 'Blog post 2',
-        'content': 'Second post contents',
-        'date_posted': 'May 6, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
-    context = {'posts': posts}
+    context = {
+        'posts': Post.objects.all()
+    }
     return render(request, 'blog/home.html', context)
 
 
