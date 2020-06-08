12974be0eab120ab9449ab4e64667ddf660e0623
Jonas G F Pettersson <jonas.pettersson@siemens.com>
2020-06-08 21:07:28 +0200

    Added STATIC_ROOT to settings.py, trying to use heroku

--- django_tutorial/django_tutorial/settings.py
+++ django_tutorial/django_tutorial/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
