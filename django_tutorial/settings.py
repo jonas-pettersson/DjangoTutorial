ad3cfda2dd65d413647028a13b00b67916f9a1af
Jonas G F Pettersson <jonas.pettersson@siemens.com>
2020-06-09 11:49:04 +0200

    django-heroku installed

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 # import json
 
 # with open('/etc/config.json') as config_file:
@@ -153,3 +154,5 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
